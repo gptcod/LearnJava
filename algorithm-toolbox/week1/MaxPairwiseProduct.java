f4ed82623ff4d7bfabba3268ea04c155cf995804
herosunly <liuyang@mail.bnu.edu.cn>
2017-12-20 08:21:31 +0800

    whole program
    add main method, static getMaxPairwiseProduct method

--- algorithm-toolbox/week1/MaxPairwiseProduct.java
+++ algorithm-toolbox/week1/MaxPairwiseProduct.java
@@ -1,30 +1,58 @@
-static class FastScanner {
-	BufferedStream br;
-	StringTokenizer st;
+import java.util.*;
+import java.io.*;
 
-	public FastScanner(InputStream stream) {
-		try {
-			br = new BufferedReader(new InputStreamReader(stream));
+public class MaxPairwiseProduct {
+    static int getMaxPairwiseProduct(int[] numbers) {
+        int result = 0;
+        int n = numbers.length;
+        for (int i = 0; i < n-1; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (numbers[i] * numbers[j] > result) {
+                    result = numbers[i] * numbers[j];
+                }
+            }
+        }
+        return result;
+    }
 
-		} catch(Exception e) {
-			e.printStackTrace();
+	public static void main(String[] args) {
+		FastScanner scanner = new FastScanner(System.in);
+		int n = scanner.nextInt();
+		int[] numbers = new int[n];
+		for (int i=0; i<n; ++i) {
+			numbers[i] = scanner.nextInt();
 		}
+
+		int result = maxPairwiseProduct(numbers);
+		System.out.println(result);
 	}
 
-	String next() {
-		while(st == null || !st.hasMoreToken()) {
-			try {
-				st = new StringTokenizer(br.readLine());
 
-			} catch(IOException e) {
+	static class FastScanner {
+		BufferedReader br;
+		StringTokenizner st;
+
+		public FastScanner(InputStream stream) {
+			try {
+				br = new BufferedReader(new InputStreamReader(stream));
+			} catch(Exception e) {
 				e.printStackTrace();
 			}
 		}
 
-		return st.nextToken();
-	}
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
 
-	int nextInt() {
-		Integer.parseInt(next());
+		int nextInt() {
+			Integer.parseInt(next());
+		}
 	}
 }
